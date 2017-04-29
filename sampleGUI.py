1bbfab100799500592d35769b8e52147b89a5a9a
somyatuteja <somya16055@iiitd.ac.in>
2017-04-30 03:04:06 +0530

    Commit-4 thoda aur GUI

--- sampleGUI.py
+++ sampleGUI.py
@@ -1,14 +1,27 @@
 import tkinter
 from tkinter import *
+import twitterstream1
 
 class TwitterGUI(Frame):
-    def __init__(self):
-        top = tkinter.Tk()
-        text= Text(top)
-        top.lab
-        text.insert(INSERT, "ENTER YOUR QUERY")
-        text.pack()
-        B1=tkinter.Button(text="Fetch Tweets",)
-        B1.pack()
-        top.mainloop()
+    def __init__(self, master=NONE):
+        Frame.__init__(self, master)
+        self.master=master
+        self.init_window()
+
+    def init_window(self):
+        text = Label(text="Enter the query").grid(row=2, column=2)
+        B1 = Button(text="Fetch Tweets", command=self.TweetFetch).grid(row=3 ,column=3)
+        self.TB1=Entry(top).grid(row=2 ,column=6)
+
+
     def TweetFetch(self):
+         self.input = self.TB1.get()
+         print(self.input)
+         ts = twitterstream1.twitterSummary()
+         ts.getSummary(self.input)
+
+top = tkinter.Tk()
+app=TwitterGUI(top)
+top.mainloop()
+
+
--- twitterstream1.py
+++ twitterstream1.py
@@ -19,51 +19,50 @@ from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 #nltk.download('punkt')
 # Variables that contains the user credentials to access Twitter API
 class twitterSummary:
-    ACCESS_TOKEN = '949253929-o6YjLSBSF6wLSqRgazQ0Dgg3g1YgaSowvVdQC0iK'
-    ACCESS_SECRET = 'cA3gerIJIssnMR83vqXSAHPpI5wloJ2DhuDlHXy97qMCO'
-    CONSUMER_KEY = 'SPpUOTLLnqlzR8roaO7adrGMi'
-    CONSUMER_SECRET = 'ZRaA1vB9FMFG6QXKZR2wDMrrjD2oZi4bTTG9TSDaqL6akzdoha'
-    LANGUAGE = 'English'
-    oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-    twitter_stream = TwitterStream(auth=oauth)
-    iterator = twitter_stream.statuses.sample()
-    inp=input("Please enter topic to get tweets")
-    text_file = open('Output.txt', 'w')
-    tweet_count = 1000
-    iterator = twitter_stream.statuses.filter(track=inp, language='en', tweet_mode='compatibility')
-
-    for tweet in iterator:
-        tweet_count -= 1
-        keys = tweet.keys()
+    def getSummary(self, inp):
+        ACCESS_TOKEN = '949253929-o6YjLSBSF6wLSqRgazQ0Dgg3g1YgaSowvVdQC0iK'
+        ACCESS_SECRET = 'cA3gerIJIssnMR83vqXSAHPpI5wloJ2DhuDlHXy97qMCO'
+        CONSUMER_KEY = 'SPpUOTLLnqlzR8roaO7adrGMi'
+        CONSUMER_SECRET = 'ZRaA1vB9FMFG6QXKZR2wDMrrjD2oZi4bTTG9TSDaqL6akzdoha'
+        LANGUAGE = 'English'
+        oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+        twitter_stream = TwitterStream(auth=oauth)
+        iterator = twitter_stream.statuses.sample()
+        text_file = open('Output.txt', 'w')
+        tweet_count = 1000
+        iterator = twitter_stream.statuses.filter(track=inp, language='en', tweet_mode='compatibility')
+        for tweet in iterator:
+            tweet_count -= 1
+            keys = tweet.keys()
   #  print(json.dumps(tweet))
-        if 'text' in tweet :
-            tweettext = tweet['text']
-            print (tweet_count)
-            cleanstring = re.sub('(@[A-Za-z0-9]:)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', tweettext)
-            c = re.sub('RT', ' ', tweettext)
-            l=c.split()
-            for word in l:
-                if(word.startswith("@")):
-                    l.remove(word)
-                if(word.startswith("http://")):
-                    l.remove(word)
-                if (word.startswith("https://")):
-                    l.remove(word)
-                if( word.startswith("&")):
-                    l.remove(word)
-            cleanString= ' '.join(l)
-            print(cleanString)
-            try:
-                text_file.write(cleanString)
-                text_file.write(".")
-            except UnicodeEncodeError:
-                tweet_count-+1
-        if tweet_count <= 0:
-            break                               
-    text_file.close()
-    print ("\n")
-    print ("SUMMARY")
-    ss = summarize.SimpleSummarizer()
-    file=open('Output.txt', 'r')
-    data=file.read()
-    print(ss.summarize(data,5,inp))
+            if 'text' in tweet :
+                tweettext = tweet['text']
+                print (tweet_count)
+                cleanstring = re.sub('(@[A-Za-z0-9]:)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', tweettext)
+                c = re.sub('RT', ' ', tweettext)
+                l=c.split()
+                for word in l:
+                    if(word.startswith("@")):
+                        l.remove(word)
+                    if(word.startswith("http://")):
+                        l.remove(word)
+                    if (word.startswith("https://")):
+                        l.remove(word)
+                    if( word.startswith("&")):
+                        l.remove(word)
+                cleanString= ' '.join(l)
+                print(cleanString)
+                try:
+                    text_file.write(cleanString)
+                    text_file.write(".")
+                except UnicodeEncodeError:
+                    tweet_count-+1
+            if tweet_count <= 0:
+                break
+        text_file.close()
+        print ("\n")
+        print ("SUMMARY")
+        ss = summarize.SimpleSummarizer()
+        file=open('Output.txt', 'r')
+        data=file.read()
+        print(ss.summarize(data,5,inp))
